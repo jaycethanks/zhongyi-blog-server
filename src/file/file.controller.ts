ea05e11dc699e9d8d598549004cd8898f7764dba
jayce <jaycethanks@outlook.com>
2022-12-10 18:18:13 +0800

    todo:file upload

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -2,13 +2,15 @@ import { Express } from 'express';
 import { diskStorage } from 'multer';
 import { join } from 'path';
 
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard';
 import {
-    Controller, Get, Ip, Param, Post, Req, Res, UploadedFile, UseInterceptors
+    Controller, Get, Ip, Param, Post, Req, Res, UploadedFile, UseGuards, UseInterceptors
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 import { FileService } from './file.service';
 
+@UseGuards(JwtAuthGuard)
 @Controller('file')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
@@ -16,7 +18,19 @@ export class FileController {
   @UseInterceptors(
     FileInterceptor('file', {
       storage: diskStorage({
-        destination: process.env.DATABASE_URL,
+        destination: (req, file, cb) => {
+          const { mimetype } = file;
+          const validFolderName = mimetype
+            .replace(/\//g, '__')
+            .replace(/\./g, '_');
+          const { userid } = req.user as any;
+          const dest =
+            process.env.NGINX_STATIC_SERVER_PATH +
+            userid +
+            '/' +
+            validFolderName;
+          cb(null, dest);
+        },
         filename: (req, file, cb) => {
           const uniqueSuffix = Date.now() + Math.round(Math.random() * 1e9);
           cb(null, uniqueSuffix + file.originalname);
@@ -24,8 +38,17 @@ export class FileController {
       }),
     }),
   )
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    // console.log('[res]: ', res);
+  uploadFile(@Req() req, @UploadedFile() file: Express.Multer.File) {
+    console.log('[file]: ', file);
+    // const { userid } = req.user
+    // const validFolderName = mimetype
+    // .replace(/\//g, '__')
+    // .replace(/\./g, '_');
+    // process.env.NGINX_STATIC_SERVER_PATH +
+    //         userid +
+    //         '/' +
+    //         validFolderName;
+
     const { filename } = file;
     return '/files/' + filename;
   }
